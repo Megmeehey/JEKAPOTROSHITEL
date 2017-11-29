ee51015bda9bdb6384863304cb203c6c2ddb3868
vladislav_mikheev <vladislav_mikheev@epam.com>
2017-11-30 00:45:16 +0300

    Forgot to turn on Optimize imports :(

--- src/test/java/quoters/TerminatorQuoterImplTest.java
+++ src/test/java/quoters/TerminatorQuoterImplTest.java
@@ -1,15 +1,11 @@
 package quoters;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 
 @Category(UnitTest.class)
 @RunWith(SpringJUnit4ClassRunner.class)
