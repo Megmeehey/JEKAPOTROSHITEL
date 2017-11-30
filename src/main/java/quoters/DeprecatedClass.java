81f3deed68979068950aceeed608a55310e0aa27
vladislav_mikheev <vladislav_mikheev@epam.com>
2017-11-30 11:03:57 +0300

    Added T1000 and DeprecatedClass annotation

---
+++ src/main/java/quoters/DeprecatedClass.java
@@ -0,0 +1,5 @@
+package quoters;
+
+public @interface DeprecatedClass {
+    Class newImpl();
+}
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import javax.annotation.PostConstruct;
 
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
@@ -15,18 +16,18 @@ public class TerminatorQuoter implements Quoter {
 
     @PostConstruct
     public void init() {
-        System.out.println("Phase 2");
+        System.out.println("Phase 2: PostConstruct method");
         System.out.println(repeat);
     }
 
     public TerminatorQuoter() {
-        System.out.println("Phase 1");
+        System.out.println("Phase 1: Constructor");
     }
 
     @Override
     @PostProxy
     public void sayQuote() {
-        System.out.println("Phase 3");
+        System.out.println("Phase 3: PostProxy method");
         for (int i = 0; i < repeat; i++) {
             System.out.println("message = " + message);
         }
