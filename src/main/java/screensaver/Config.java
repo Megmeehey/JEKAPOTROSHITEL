731a59ce23d3cce3d8abc6b5e0b3b68c0e5778a5
vladislav_mikheev <vladislav_mikheev@epam.com>
2017-11-30 20:05:39 +0300

    Scope!

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -3,6 +3,7 @@ package screensaver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.awt.*;
 import java.util.Random;
@@ -11,6 +12,7 @@ import java.util.Random;
 @ComponentScan(basePackages = "screensaver")
 public class Config {
     @Bean
+    @Scope(value="prototype")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
